fd734e7cb385097ec1f5855fa06ba73e0cc50713
Benton-Michael <mbenton002@gmail.com>
2023-08-04 18:57:22 -0700

    Py Bank Account

--- Week_2/Core_assign/Bank_Account/bank_account.py
+++ Week_2/Core_assign/Bank_Account/bank_account.py
@@ -15,3 +15,27 @@
 # Return the object instance (self) in methods and test by chaining method calls
 
 # Test the functionality of your class by creating instances and calling methods with different test data and scenarios
+
+class BankAccount:
+    accounts = []
+
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.accounts.append(self)
+
+    def despoit(self, amount):
+        self.balance =+ amount
+        print(f'Desposit: {amount} New Balance: {self.balance}')
+        return self
+    
+    def withdraw(self, amount):
+        if ( (self.balance - amount) < 0  ):
+            print('Insufficient funds. Charging a $5:00 fee')
+            self.balance -= (amount + 5)
+            print(f'Withdrawl ${amount} Your new balance with fee is ${self.balance} ')
+            return self
+        else:
+            self.balance -= amount
+            print(f'Withdrawl: $ {amount} Your New Balance is: {self.balance}')
+            return self
